f822ba5012ba0662fd0ec413c933934523ae4715
Gowthamj7773 <gowthamj7773@gmail.com>
2025-08-29 09:06:30 +0530

    updated

--- mess_manager_role/app/(tabs)/_layout.tsx
+++ mess_manager_role/app/(tabs)/_layout.tsx
@@ -6,13 +6,11 @@ const homeOutline = require('../../assets/icons/homeOutline.png');
 
 const chatPressed = require('../../assets/icons/chats.png');
 const chatOutline = require('../../assets/icons/chatOutline.png');
-
 export default function _layout() {
     return (
         <Tabs screenOptions={{headerShown:false, tabBarActiveTintColor:'#4A5B9B'}}>
             <Tabs.Screen name='home' options={{tabBarIcon:({color,focused})=>(<Image source={focused ? homePressed: homeOutline} className='w-5 h-5' style={{tintColor:color}}/>)}}/>
             <Tabs.Screen name='Chats' options={{tabBarIcon:({color,focused})=>(<Image source={focused ? chatPressed : chatOutline} className='w-6 h-5' style={{tintColor:color}}/>)}}/>
-
         </Tabs>
-    )
+        )
 }
